5ac90065f780f6dddb18220819fb1dd6b68691c8
jayatemi <jonathanatemi@gmail.com>
2024-04-10 22:50:59 -0400

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   #create custom routes for portfolio item
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
